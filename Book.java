7d97adc5d4b5934f6676431c3db5b14a5bdde8ee
Pavithra-Ponraj <49715927+Pavithra-Ponraj@users.noreply.github.com>
2019-10-11 11:20:48 +0530

    delete

--- Book.java
+++
@@ -1,9 +0,0 @@
-
-public class Book extends WrittenItem{
-
-	public Book(int bookId, String title, int noOfCopies) {
-		super(bookId, title, noOfCopies);
-		// TODO Auto-generated constructor stub
-	}
-
-}
